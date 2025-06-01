32d9ac8aef8372f50b516a838db4054bb12ee415
MrSlntGhost <isaegenuet@gmail.com>
2025-06-01 13:59:40 +0200

    Refactor directory structure and update paths in configuration files

--- lib/init.sh
+++ lib/init.sh
@@ -4,7 +4,7 @@ ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 LIB_DIR="$ROOT_DIR/lib"
 ACORE_DIR="$ROOT_DIR/azerothcore-wotlk"
 MODULES_DIR="$ACORE_DIR/modules"
-CUSTOM_SQL_DIR="$ROOT_DIR/env/data/sql/custom"
+CUSTOM_SQL_DIR="$ROOT_DIR/data/sql/custom"
 
 LINE_LONG="-----------------------------------------------------"
 LINE_SHORT="----"
